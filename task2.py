cbf8edc99f6108a262236f9b62dcbec5e5157373
vova4039 <kuraev-2009@mail.ru>
2025-04-22 11:00:32 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,87 @@
+# #create
+# a='привет'
+# b="привет"
+# c="я'знаю python"
+# d='я"знаю"Python'
+#
+#
+# a=123
+# a=str(a)
+# str([1,1.1,'a'])
+# str(True)
+# str(None)
+# a='привет'
+# b='Иван'
+# c=f"{a}я{b}"
+# #retrive(Read)прочитать
+# a="привет"
+# print(a)
+# print("Иван")
+# a="привет"
+# a[0]
+# a[1]
+# a[2]
+# a[3]
+# a[4]
+# a[5]
+# a[-1]
+# a[-2]
+# a[-3]
+# a[-4]
+# a[-5]
+# a[-6]
+# #update \ delete
+# a="привет"
+# a[0]='б'# хотели получить бривет а получили оширбку что строку нельзя изменять
+# a="бривет"
+# a='привет'
+# del a[0]
+# del a
+
+print(f'ЗАДАЧА 1')
+length = 90
+width = 50
+perimeter = 2*(90+50)
+print(perimeter)
+
+print(f'ЗАДАЧА 2')
+maney = 10000
+add = 10000+5000
+print(add)
+
+print(f'ЗАДАЧА 3')
+hours = 5000
+days = 5000//24
+hour_1 = hours % days
+
+print(days)
+print(hour_1)
+
+print('ЗАДАЧА 4')
+pages = 100
+lines = 50
+symbol = 25
+sumbol_1 = 4
+volume = 1.44 * 1024 * 1024
+book = sumbol_1 * symbol * lines * pages
+book_2 = volume // book
+
+print(book_2)
+
+print(f'ЗАДАЧА 5')
+r = 5
+pi = 3.1415
+side = 5
+circl = 2 * pi * r
+p = 4 * side
+squar = side ** 2
+sq_circl = pi * (r ** 2)
+print(f'{circl:.2f}')
+print(squar)
+print(p)
+print(f'{sq_circl:.2f}')
+
+print(f'ЗАДАЧА 6')
+str_numbers = 20 * '0' + 50 * '1' + 30 * '2'
+print(str_numbers)
+
